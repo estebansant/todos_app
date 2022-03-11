ab56b4e64cf90338dd78150fc76d65bd2d44fc9c
estebansant <estebansant30@gmail.com>
2022-03-10 21:19:11 -0300

    Updates

---
+++ precache-manifest.80d6431ace560559ed63be36daad9b95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4707647754de85dac7eb",
+    "url": "/todos_app/static/css/main.14424e2d.chunk.css"
+  },
+  {
+    "revision": "4707647754de85dac7eb",
+    "url": "/todos_app/static/js/main.47076477.chunk.js"
+  },
+  {
+    "revision": "a09ff8ec859b2a5284c6",
+    "url": "/todos_app/static/js/1.a09ff8ec.chunk.js"
+  },
+  {
+    "revision": "241ccd742c0b57e0df10",
+    "url": "/todos_app/static/js/runtime~main.241ccd74.js"
+  },
+  {
+    "revision": "807561be727036baa665eccc91e46b4f",
+    "url": "/todos_app/index.html"
+  }
+];
